e40069214364cc639acf193b2abb4cb1263379ff
Pradeep Vemulakonda <pradeep.vemulakonda@orionhealth.com>
2015-03-09 00:27:35 +1100

    Updated to open camera

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,51 +23,50 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
+  $stateProvider.state('app', {
+    url: '/app',
     abstract: true,
-    templateUrl: "templates/menu.html",
+    templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: "/search",
+  }).state('app.search', {
+    url: '/search',
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: 'templates/search.html'
       }
     }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
+  }).state('app.browse', {
+    url: '/browse',
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: 'templates/browse.html'
       }
     }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
+  }).state('app.receipts', {
+      url: '/receipts',
       views: {
         'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
+          templateUrl: 'templates/receipts.html',
+          controller: 'ReceiptsCtrl'
         }
       }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
+    }).state('app.single', {
+    url: '/receipts/:receiptId',
     views: {
       'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
+        templateUrl: 'templates/receipt.html',
+        controller: 'ReceiptCtrl'
       }
     }
-  });
+  }).state('app.snap', {
+        url: '/snap',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/snap.html',
+            controller: 'SnapCtrl'
+          }
+        }
+      });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/receipts');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,25 +24,65 @@ angular.module('starter.controllers', [])
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
+    $scope.modal.hide();
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('ReceiptsCtrl', function($scope) {
+  $scope.receipts = [
+    { title: 'Receipt 1', id: 1 },
+    { title: 'Receipt 2', id: 2 },
+    { title: 'Receipt 3', id: 3 },
+    { title: 'Receipt 4', id: 4 },
+    { title: 'Receipt 5', id: 5 },
+    { title: 'Receipt 6', id: 6 }
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('ReceiptCtrl', function($scope, $stateParams) {
+})
+
+.controller('SnapCtrl', function($scope, Camera, $ionicHistory, $state) {
+    $scope.getPhoto = function() {
+        console.log('Getting camera');
+        Camera.getPicture().then(function(imageURI) {
+          console.log(imageURI);
+          $scope.lastPhoto = imageURI;
+        }, function(err) {
+          console.err(err);
+        }, {
+          quality: 75,
+          targetWidth: 320,
+          targetHeight: 320,
+          saveToPhotoAlbum: true
+        });
+        /*
+        navigator.camera.getPicture(function(imageURI) {
+          console.log(imageURI);
+        }, function(err) {
+        }, {
+          quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL
+        });
+        */
+    };
+
+    $scope.extractData = function() {
+      alert($scope.lastPhoto);
+    };
+
+    $scope.cancel = function() {
+      $ionicHistory.nextViewOptions({
+        disableBack: true
+      });
+
+      $state.go('app.receipts');
+    };
+
+
 });
+
+
